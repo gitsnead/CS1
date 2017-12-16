4784beb34e02fa26cf18415f01f469e4861622be
Ryan Snead <ryan.w.snead@gmail.com>
2017-12-16 09:16:42 -0500

    Completed exercise 9.

--- 2013-OOProgrammingWithJava-PART1/week1-009.Divider/src/Divider.java
+++ 2013-OOProgrammingWithJava-PART1/week1-009.Divider/src/Divider.java
@@ -7,5 +7,13 @@ public class Divider {
         Scanner reader = new Scanner(System.in);
 
         // Implement your program here. Remember to ask the input from user.
+        System.out.print("Type a number: ");
+        Scanner reader1 = new Scanner(System.in);
+        int firstNumber = Integer.parseInt(reader1.nextLine());
+        System.out.print("Type another number: ");
+        int secondNumber = Integer.parseInt(reader1.nextLine());
+        double quotient = (double)firstNumber/secondNumber;
+        System.out.println("");
+        System.out.println("Division: " + firstNumber + " / " + secondNumber + " = " + (quotient));
     }
 }
