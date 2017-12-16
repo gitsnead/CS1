d35eb6e9b6e6e6daa1a696ffd1969808bfa2fff9
Ryan Snead <ryan.w.snead@gmail.com>
2017-12-16 09:45:45 -0500

    Completed exercise 12.

--- 2013-OOProgrammingWithJava-PART1/week1-012.SumOfTheAges/src/SumOfTheAges.java
+++ 2013-OOProgrammingWithJava-PART1/week1-012.SumOfTheAges/src/SumOfTheAges.java
@@ -7,5 +7,15 @@ public class SumOfTheAges {
         Scanner reader = new Scanner(System.in);
 
         // Implement your program here
+        System.out.print("Type your name: ");
+        String firstPersonName = reader.nextLine();
+        System.out.print("Type your age: ");
+        int firstAge = Integer.parseInt(reader.nextLine());
+        System.out.print("Type your name: ");
+        String secondPersonName = reader.nextLine();
+        System.out.print("Type your age: ");
+        int secondAge = Integer.parseInt(reader.nextLine());
+        System.out.println("");
+        System.out.println(firstPersonName + " and " + secondPersonName + " are " + (firstAge + secondAge) + " years old in total.");
     }
 }
