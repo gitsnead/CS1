81a8ff2a3ea2345e01bfd84dbfd724e699bc3699
Ryan Snead <ryan.w.snead@gmail.com>
2017-12-16 10:03:41 -0500

    Completed exercise 13.

--- 2013-OOProgrammingWithJava-PART1/week1-013.NhlStatisticsPart1/src/Main.java
+++ 2013-OOProgrammingWithJava-PART1/week1-013.NhlStatisticsPart1/src/Main.java
@@ -1,8 +1,26 @@
-
+import java.util.Scanner;
 import nhlstats.NHLStatistics;
 
 public class Main {
 
     public static void main(String[] args) {
+        
+        System.out.println("Top ten by goals");
+        NHLStatistics.sortByGoals();
+        NHLStatistics.top(10);
+        System.out.println("");
+        System.out.println("Top twenty-five by penalty");
+        NHLStatistics.sortByPenalties();
+        NHLStatistics.top(25);
+        System.out.println("");
+        System.out.println("Sydney Crosby statistics");
+        NHLStatistics.searchByPlayer("Sidney Crosby");
+        System.out.println("");
+        System.out.println("Players in Philadelphia Flyers (sorted by penalty)");
+        NHLStatistics.teamStatistics("PHI");
+        System.out.println("");
+        System.out.println("Players in Anaheim Ducks (sorted by points)");
+        NHLStatistics.sortByPoints();
+        NHLStatistics.teamStatistics("ANA");
     }
 }
