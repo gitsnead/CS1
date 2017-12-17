97019326c13f673ad00336e65daef60f9c02a674
Ryan Snead <ryan.w.snead@gmail.com>
2017-12-17 08:33:04 -0500

    Completed exercise 22.

--- 2013-OOProgrammingWithJava-PART1/week1-022.Password/src/Password.java
+++ 2013-OOProgrammingWithJava-PART1/week1-022.Password/src/Password.java
@@ -8,5 +8,16 @@ public class Password {
         String password = "carrot"; // Use carrot as password when running tests.
 
         // Write your code here
+        while (true) {
+            System.out.print("Type the password: ");
+            String guess = reader.nextLine();
+            if (guess.equals(password))  {
+                System.out.println("Right!");
+                break;
+            } else {
+                System.out.println("Wrong!");
+            }
+        }
+        System.out.println("The secret is: jryy qbar!");
     }
 }
