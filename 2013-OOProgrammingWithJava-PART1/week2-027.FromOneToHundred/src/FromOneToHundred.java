664a2a1bbea09319f5dad3d2d90c65344bbd6720
Ryan Snead <ryan.w.snead@gmail.com>
2017-12-17 12:55:02 -0500

    Completed exercise 27.

--- 2013-OOProgrammingWithJava-PART1/week2-027.FromOneToHundred/src/FromOneToHundred.java
+++ 2013-OOProgrammingWithJava-PART1/week2-027.FromOneToHundred/src/FromOneToHundred.java
@@ -3,5 +3,10 @@ public class FromOneToHundred {
 
     public static void main(String[] args) {
         // Write your program here
+        int counter = 0;
+        while (counter < 100) {
+            counter++;
+            System.out.println(counter);
+        }
     }
 }
