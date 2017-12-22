b44f4a7e4ff2d400797841195fc4a0f44cc48b37
Ryan Snead <ryan.w.snead@gmail.com>
2017-12-22 18:38:38 -0500

    Completed exercise 29.

--- 2013-OOProgrammingWithJava-PART1/week2-029.EvenNumbers/src/EvenNumbers.java
+++ 2013-OOProgrammingWithJava-PART1/week2-029.EvenNumbers/src/EvenNumbers.java
@@ -3,5 +3,12 @@ public class EvenNumbers {
 
     public static void main(String[] args) {
         // Write your code here
+        int counter = 1;
+        while (counter <= 100) {
+            if (counter % 2 == 0) {
+                System.out.println(counter);
+            }
+            counter++;
+        }
     }
 }
