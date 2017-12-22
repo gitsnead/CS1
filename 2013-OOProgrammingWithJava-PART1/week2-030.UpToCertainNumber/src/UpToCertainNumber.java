40ea147cdc825f6e92c2886f493c3a6368d01775
Ryan Snead <ryan.w.snead@gmail.com>
2017-12-22 18:46:12 -0500

    Completed exercise 30.

--- 2013-OOProgrammingWithJava-PART1/week2-030.UpToCertainNumber/src/UpToCertainNumber.java
+++ 2013-OOProgrammingWithJava-PART1/week2-030.UpToCertainNumber/src/UpToCertainNumber.java
@@ -8,6 +8,12 @@ public class UpToCertainNumber {
         Scanner reader = new Scanner(System.in);
         
         // Write your code here
-        
+        int counter = 1;
+        System.out.print("Up to what number? ");
+        int maximum = Integer.parseInt(reader.nextLine());
+        while(counter <= maximum) {
+            System.out.println(counter);
+            counter++;
+        }
     }
 }
