6b21f1049e96536ffd5c97149d6ade3930cb0ec4
Ryan Snead <ryan.w.snead@gmail.com>
2017-12-24 08:50:45 -0500

    Completed exercise 34.

--- 2013-OOProgrammingWithJava-PART1/week2-034.Factorial/src/Factorial.java
+++ 2013-OOProgrammingWithJava-PART1/week2-034.Factorial/src/Factorial.java
@@ -3,6 +3,14 @@ import java.util.Scanner;
 public class Factorial {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-
+        System.out.print("Type a number: ");
+        int Maximum = Integer.parseInt(reader.nextLine());
+        int Counter = 1;
+        int Factorial = 1;
+        while (Counter <= Maximum) {
+            Factorial *= Counter;
+            Counter++;
+        }
+        System.out.println("Factorial is " + Factorial);
     }
 }
