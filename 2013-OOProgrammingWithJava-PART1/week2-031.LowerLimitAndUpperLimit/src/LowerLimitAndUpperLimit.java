71295d131c2b438e09319dc4a17178e13df46e24
Ryan Snead <ryan.w.snead@gmail.com>
2017-12-24 06:05:40 -0500

    Completed exercise 31.

--- 2013-OOProgrammingWithJava-PART1/week2-031.LowerLimitAndUpperLimit/src/LowerLimitAndUpperLimit.java
+++ 2013-OOProgrammingWithJava-PART1/week2-031.LowerLimitAndUpperLimit/src/LowerLimitAndUpperLimit.java
@@ -7,6 +7,14 @@ public class LowerLimitAndUpperLimit {
         Scanner reader = new Scanner(System.in);
 
         // write your code here
-
+        System.out.print("First: ");
+        int First = Integer.parseInt(reader.nextLine());
+        System.out.print("Last: ");
+        int Last = Integer.parseInt(reader.nextLine());
+        int counter = First;
+        while(counter <= Last) {
+            System.out.println(counter);
+            counter++;
+        }
     }
 }
