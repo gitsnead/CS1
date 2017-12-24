b504038eb73062fe1faf9d9466ef345dc5f51a23
Ryan Snead <ryan.w.snead@gmail.com>
2017-12-24 06:17:32 -0500

    Completed exercise 32.

--- 2013-OOProgrammingWithJava-PART1/week2-032.TheSumOfSetOfNumbers/src/TheSumOfSetOfNumbers.java
+++ 2013-OOProgrammingWithJava-PART1/week2-032.TheSumOfSetOfNumbers/src/TheSumOfSetOfNumbers.java
@@ -5,6 +5,14 @@ public class TheSumOfSetOfNumbers {
 
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-
+        System.out.print("Until what? ");
+        int What = Integer.parseInt(reader.nextLine());
+        int counter = 0;
+        int Total = 0;
+        while(counter <= What) {
+            Total += counter;
+            counter++;
+        }
+        System.out.println("Sum is " + Total);
     }
 }
