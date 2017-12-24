8654e0d800d8c70fab29c5b749c7afa68681bf80
Ryan Snead <ryan.w.snead@gmail.com>
2017-12-24 08:57:32 -0500

    Completed exercise 35.

--- 2013-OOProgrammingWithJava-PART1/week2-035.SumOfThePowers/src/SumOfThePowers.java
+++ 2013-OOProgrammingWithJava-PART1/week2-035.SumOfThePowers/src/SumOfThePowers.java
@@ -5,6 +5,14 @@ public class SumOfThePowers {
 
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-
+        System.out.print("Type a number: ");
+        int Maximum = Integer.parseInt(reader.nextLine());
+        int Counter = 0;
+        int Result = 0;
+        while (Counter <= Maximum) {
+            Result += Math.pow(2, Counter);
+            Counter++;
+        }
+        System.out.println("Result is " + Result);
     }
 }
