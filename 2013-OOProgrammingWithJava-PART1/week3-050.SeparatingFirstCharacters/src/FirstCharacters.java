617165658ac9090d9f46c080b6126bab3f51f095
Ryan Snead <ryan.w.snead@gmail.com>
2017-12-26 10:11:27 -0500

    Completed exercise 50.

--- 2013-OOProgrammingWithJava-PART1/week3-050.SeparatingFirstCharacters/src/FirstCharacters.java
+++ 2013-OOProgrammingWithJava-PART1/week3-050.SeparatingFirstCharacters/src/FirstCharacters.java
@@ -1,7 +1,16 @@
+
 import java.util.Scanner;
 
 public class FirstCharacters {
+
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
+        System.out.print("Type your name: ");
+        String name = reader.nextLine();
+        Integer index = 0;
+        while (index < 3 && name.length() > 2) {
+            System.out.println((index + 1) + ". character: " + name.charAt(index));
+            index++;
+        }
     }
 }
