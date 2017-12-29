12a36dad010e19c450634ac8457871759c9b8d03
Ryan Snead <ryan.w.snead@gmail.com>
2017-12-29 10:08:33 -0500

    Completed exercise 67.

--- 2013-OOProgrammingWithJava-PART1/week3-067.TheVariance/src/Variance.java
+++ 2013-OOProgrammingWithJava-PART1/week3-067.TheVariance/src/Variance.java
@@ -1,28 +1,46 @@
+
 import java.util.ArrayList;
+import java.math.*;
 
 public class Variance {
+
     // Copy here sum from exercise 63 
     public static int sum(ArrayList<Integer> list) {
-        return 0;
+        // Write your code here
+        int listTotal = 0;
+        for(int listItem : list) {
+            listTotal += listItem;
+        }
+        return listTotal;
     }
-    
+
     // Copy here average from exercise 64 
     public static double average(ArrayList<Integer> list) {
-        return 0;
+        // write code here
+        return sum(list) / (list.size() + 0.0d);
     }
 
     public static double variance(ArrayList<Integer> list) {
         // write code here
-        return 0;
+        double avg = average(list);
+        double sampleVariance = 0;
+        for (int item : list) {
+            sampleVariance += Math.pow((item - avg), 2);
+        }
+        if (list.size() == 1) {
+            return 0;
+        } else {
+            return sampleVariance / (list.size() - 1);
+        }
     }
-    
+
     public static void main(String[] args) {
         ArrayList<Integer> list = new ArrayList<Integer>();
         list.add(3);
         list.add(2);
         list.add(7);
         list.add(2);
-        
+
         System.out.println("The variance is: " + variance(list));
     }
 
