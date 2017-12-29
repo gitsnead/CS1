c05bc96851910e027e10ec6e3826dc6667aa9f80
Ryan Snead <ryan.w.snead@gmail.com>
2017-12-29 06:36:59 -0500

    Completed exercise 51.

--- 2013-OOProgrammingWithJava-PART1/week3-051.SeparatingCharacters/src/SeparatingCharacters.java
+++ 2013-OOProgrammingWithJava-PART1/week3-051.SeparatingCharacters/src/SeparatingCharacters.java
@@ -5,5 +5,12 @@ public class SeparatingCharacters {
 
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
+        System.out.print("Type your name: ");
+        String name = reader.nextLine();
+        Integer index = 0;
+        while (index < name.length()) {
+            System.out.println((index + 1) + ". character: " + name.charAt(index));
+            index++;
+        }        
     }
 }
