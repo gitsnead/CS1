d95957bf33752fd99ab31336da679a595823cf7d
Ryan Snead <ryan.w.snead@gmail.com>
2017-12-29 08:42:33 -0500

    Completed exercise 60.

--- 2013-OOProgrammingWithJava-PART1/week3-060.WordsInAlphabeticalOrder/src/WordsInAlphabeticalOrder.java
+++ 2013-OOProgrammingWithJava-PART1/week3-060.WordsInAlphabeticalOrder/src/WordsInAlphabeticalOrder.java
@@ -8,5 +8,21 @@ public class WordsInAlphabeticalOrder {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
         // create here an ArrayList
+        ArrayList<String> words = new ArrayList<String>();
+        String word;
+        while (true) {
+            System.out.print("Type a word: ");
+            word = reader.nextLine();
+            if (word.isEmpty()) {
+                break;
+            } else {
+                words.add(word);
+            }
+        }
+        Collections.sort(words);
+        System.out.println("You typed the following words:");
+        for (String item : words) {
+            System.out.println(item);
+        }
     }
 }
