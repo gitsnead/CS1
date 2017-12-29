1cfb9830f1632444b22c6a2ed1db297da3194ec4
Ryan Snead <ryan.w.snead@gmail.com>
2017-12-29 06:59:01 -0500

    Completed exercise 53.

--- 2013-OOProgrammingWithJava-PART1/week3-053.FirstPart/src/FirstPart.java
+++ 2013-OOProgrammingWithJava-PART1/week3-053.FirstPart/src/FirstPart.java
@@ -5,5 +5,10 @@ public class FirstPart {
 
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
+        System.out.print("Type a word: ");
+        String word = reader.nextLine();
+        System.out.print("Length of the first part: ");
+        int charsToPrint = Integer.parseInt(reader.nextLine());
+        System.out.println("Result: " + word.substring(0,charsToPrint));
     }
 }
