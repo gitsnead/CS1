10a9444794477095e767b3774976c49421cbfe36
Ryan Snead <ryan.w.snead@gmail.com>
2017-12-29 07:43:18 -0500

    Completed exercise 56.

--- 2013-OOProgrammingWithJava-PART1/week3-056.ReversingText/src/ReversingText.java
+++ 2013-OOProgrammingWithJava-PART1/week3-056.ReversingText/src/ReversingText.java
@@ -5,8 +5,14 @@ public class ReversingText {
 
     public static String reverse(String text) {
         // write your code here
-        // note that method does now print anything, it RETURNS the reversed string
-        return "";
+        // note that method does not print anything, it RETURNS the reversed string
+        int index = text.length()-1;
+        String reverseText = "";
+        while (index >= 0) {
+            reverseText = reverseText + text.substring(index, index + 1);
+            index -= 1;
+        }
+        return reverseText;
     }
 
     public static void main(String[] args) {
