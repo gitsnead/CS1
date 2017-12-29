177a0630910ee7971cb23eb51517c323f7dc91a7
Ryan Snead <ryan.w.snead@gmail.com>
2017-12-29 07:04:27 -0500

    Completed exercise 54.

--- 2013-OOProgrammingWithJava-PART1/week3-054.TheEndPart/src/TheEndPart.java
+++ 2013-OOProgrammingWithJava-PART1/week3-054.TheEndPart/src/TheEndPart.java
@@ -3,5 +3,12 @@ import java.util.Scanner;
 public class TheEndPart {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
+        System.out.print("Type a word: ");
+        String word = reader.nextLine();
+        System.out.print("Length of the end part: ");
+        int charsToPrint = Integer.parseInt(reader.nextLine());
+        if (word.length() > charsToPrint) {
+            System.out.println("Result: " + word.substring(word.length() - charsToPrint));
+        }
     }
 }
