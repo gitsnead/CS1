a1728c0e1e195a97e472de30c167ad47516338bd
Ryan Snead <ryan.w.snead@gmail.com>
2017-12-29 06:45:23 -0500

    Completed exercise 52.

--- 2013-OOProgrammingWithJava-PART1/week3-052.ReversingName/src/ReversingName.java
+++ 2013-OOProgrammingWithJava-PART1/week3-052.ReversingName/src/ReversingName.java
@@ -3,5 +3,13 @@ import java.util.Scanner;
 public class ReversingName {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
+        System.out.print("Type your name: ");
+        String name = reader.nextLine();
+        Integer index = name.length();
+        while (index > 0) {
+            System.out.print(name.charAt(index-1));
+            index -= 1;
+        }
+        System.out.println("");
     }
 }
