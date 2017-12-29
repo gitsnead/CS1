7e6ba2e24c4411ee4c8a4e4693422765bb11c910
Ryan Snead <ryan.w.snead@gmail.com>
2017-12-29 08:39:32 -0500

    Completed exercise 59.

--- 2013-OOProgrammingWithJava-PART1/week3-059.WordsInReverseOrder/src/WordsInReverseOrder.java
+++ 2013-OOProgrammingWithJava-PART1/week3-059.WordsInReverseOrder/src/WordsInReverseOrder.java
@@ -6,5 +6,21 @@ public class WordsInReverseOrder {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
         // create here an ArrayList
+        ArrayList<String> words = new ArrayList<String>();
+        String word;
+        while (true) {
+            System.out.print("Type a word: ");
+            word = reader.nextLine();
+            if (word.isEmpty()) {
+                break;
+            } else {
+                words.add(word);
+            }
+        }
+        Collections.reverse(words);
+        System.out.println("You typed the following words:");
+        for (String item : words) {
+            System.out.println(item);
+        }
     }
 }
