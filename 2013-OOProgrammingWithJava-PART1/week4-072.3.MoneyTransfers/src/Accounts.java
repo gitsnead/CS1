519dbdd671a23e4c28832322542741248f74a437
Ryan Snead <ryan.w.snead@gmail.com>
2017-12-31 08:50:17 -0500

    Completed exercise 72.3.

--- 2013-OOProgrammingWithJava-PART1/week4-072.3.MoneyTransfers/src/Accounts.java
+++ 2013-OOProgrammingWithJava-PART1/week4-072.3.MoneyTransfers/src/Accounts.java
@@ -1,9 +1,19 @@
 
 public class Accounts {
 
+    public static void transfer(Account from, Account to, double howMuch) {
+        from.withdrawal(howMuch);
+        to.deposit(howMuch);
+    }
+    
     public static void main(String[] args) {
         // Code in Account.Java should not be touched!
         // write your code here
+        Account accountA = new Account("A", 100d);
+        Account accountB = new Account("B", 0d);
+        Account accountC = new Account("C", 0d);
+        transfer(accountA, accountB, 50d);
+        transfer(accountB, accountC, 25d);
     }
 
 
