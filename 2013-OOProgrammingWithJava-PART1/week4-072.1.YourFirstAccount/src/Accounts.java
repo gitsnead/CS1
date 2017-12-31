9d1c2cc4de3aa2a748669934a0f04c780d224acb
Ryan Snead <ryan.w.snead@gmail.com>
2017-12-31 08:36:42 -0500

    Completed exercise 72.1.

--- 2013-OOProgrammingWithJava-PART1/week4-072.1.YourFirstAccount/src/Accounts.java
+++ 2013-OOProgrammingWithJava-PART1/week4-072.1.YourFirstAccount/src/Accounts.java
@@ -6,6 +6,9 @@ public class Accounts {
     public static void main(String[] args) {
         // Code in Account.Java should not be touched!
         // write your code here
+        Account myAccount = new Account("me", 100d);
+        myAccount.deposit(20d);
+        System.out.println(myAccount.toString());
     }
 
 }
