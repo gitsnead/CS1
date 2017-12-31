7a1c129d5347883f9ea33cfb6134dd4a908f8ea4
Ryan Snead <ryan.w.snead@gmail.com>
2017-12-31 07:45:47 -0500

    Completed exercise 69.

--- 2013-OOProgrammingWithJava-PART1/week3-069.Palindrome/src/Palindromi.java
+++ 2013-OOProgrammingWithJava-PART1/week3-069.Palindrome/src/Palindromi.java
@@ -2,9 +2,21 @@ import java.util.Scanner;
 
 public class Palindromi {
 
+        public static String reverse(String text) {
+        // write your code here
+        // note that method does not print anything, it RETURNS the reversed string
+        int index = text.length()-1;
+        String reverseText = "";
+        while (index >= 0) {
+            reverseText = reverseText + text.substring(index, index + 1);
+            index -= 1;
+        }
+        return reverseText;
+    }
+        
     public static boolean palindrome(String text) {
         // write code here
-        return false;
+        return (text.equalsIgnoreCase(reverse(text)));
     }
 
     public static void main(String[] args) {
