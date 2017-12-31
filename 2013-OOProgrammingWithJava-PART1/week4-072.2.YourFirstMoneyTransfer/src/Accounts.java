d4d6c54ab84ab6ce9203674cbf5550b8e9351301
Ryan Snead <ryan.w.snead@gmail.com>
2017-12-31 08:42:18 -0500

    Completed exercise 72.2.

--- 2013-OOProgrammingWithJava-PART1/week4-072.2.YourFirstMoneyTransfer/src/Accounts.java
+++ 2013-OOProgrammingWithJava-PART1/week4-072.2.YourFirstMoneyTransfer/src/Accounts.java
@@ -4,6 +4,12 @@ public class Accounts {
     public static void main(String[] args) {
         // Code in Account.Java should not be touched!
         // write your code here
+        Account mattsMoney = new Account("Matt's account", 1000d);
+        Account myMoney = new Account("My account", 0d);
+        mattsMoney.withdrawal(100d);
+        myMoney.deposit(100d);
+        System.out.println(mattsMoney.toString());
+        System.out.println(myMoney.toString());
     }
 
 }
