932d14b3d2a700b83a068a752242e7535e190895
Ryan Snead <ryan.w.snead@gmail.com>
2018-01-01 07:26:23 -0500

    Completed exercise 81.

--- 2013-OOProgrammingWithJava-PART1/week4-081.PasswordRandomizer/src/PasswordRandomizer.java
+++ 2013-OOProgrammingWithJava-PART1/week4-081.PasswordRandomizer/src/PasswordRandomizer.java
@@ -2,13 +2,24 @@ import java.util.Random;
 
 public class PasswordRandomizer {
     // Define the variables
+    private Random random = new Random();
+    private int passwordLength;
+    private final String alphabet = "abcdefghijklmnopqrstuvwxyz";
 
     public PasswordRandomizer(int length) {
         // Initialize the variable
+        random = new Random();
+        this.passwordLength = length;
     }
 
     public String createPassword() {
         // write code that returns a randomized password
-        return "";
+        int characterPosition = 0;
+        String password = "";
+        while (characterPosition < this.passwordLength) {
+            password += alphabet.charAt(random.nextInt(alphabet.length()));
+            characterPosition++;
+        }
+        return password;
     }
 }
