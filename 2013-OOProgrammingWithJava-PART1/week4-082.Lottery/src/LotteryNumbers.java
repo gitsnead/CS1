735e7bf63209aaf8f0a8cc303cf8d13143be9e11
Ryan Snead <ryan.w.snead@gmail.com>
2018-01-01 08:05:53 -0500

    Completed exercise 82.

--- 2013-OOProgrammingWithJava-PART1/week4-082.Lottery/src/LotteryNumbers.java
+++ 2013-OOProgrammingWithJava-PART1/week4-082.Lottery/src/LotteryNumbers.java
@@ -1,10 +1,16 @@
+
 import java.util.ArrayList;
 import java.util.Random;
 
 public class LotteryNumbers {
+
     private ArrayList<Integer> numbers;
+    private Random random = new Random();
+    private final int ballCount = 39;
 
     public LotteryNumbers() {
+        // We'll format a list for the numbers
+        this.numbers = new ArrayList<Integer>();
         // Draw numbers as LotteryNumbers is created
         this.drawNumbers();
     }
@@ -17,10 +23,22 @@ public class LotteryNumbers {
         // We'll format a list for the numbers
         this.numbers = new ArrayList<Integer>();
         // Write the number drawing here using the method containsNumber()
+        int numberDraw;
+        int ball = 0;
+        while (ball < 7) {
+            while (true) {
+                numberDraw = random.nextInt(ballCount) + 1;
+                if (!this.containsNumber(numberDraw)) {
+                    this.numbers.add(numberDraw);
+                    break;
+                }
+            }
+            ball++;
+        }
     }
 
     public boolean containsNumber(int number) {
-        // Test here if the number is already in the drawn numbers
-        return true;
+        // Test here if the number is already among the drawn numbers
+        return this.numbers.contains(number);
     }
 }
