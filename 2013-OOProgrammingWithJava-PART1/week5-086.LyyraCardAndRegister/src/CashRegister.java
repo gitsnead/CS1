bdfc121039bccb4f09114dd84242a3ae3e2d2333
Ryan Snead <ryan.w.snead@gmail.com>
2018-01-06 09:34:02 -0500

    Completed exercise 86.

--- 2013-OOProgrammingWithJava-PART1/week5-086.LyyraCardAndRegister/src/CashRegister.java
+++ 2013-OOProgrammingWithJava-PART1/week5-086.LyyraCardAndRegister/src/CashRegister.java
@@ -4,9 +4,28 @@ public class CashRegister {
     private double cashInRegister;
     private int economicalSold;
     private int gourmetSold;
+    private final double economyPrice = 2.5d;
+    private final double gourmetPrice = 4d;
 
     public CashRegister() {
-        // at start the register has 1000 euros 
+        // at start the register has 1000 euros
+        this.cashInRegister = 1000d;
+        this.economicalSold = 0;
+        this.gourmetSold = 0;
+    }
+
+    private double payCash(double amountPaid, double price) {
+        this.cashInRegister += price;
+        return amountPaid - price;
+    }
+
+    public void loadMoneyToCard(LyyraCard card, double sum) {
+        if (sum > 0) {
+            double change = payCash(sum, sum);
+            if (change == 0) {
+                card.loadMoney(sum);
+            }
+        }
     }
 
     public double payEconomical(double cashGiven) {
@@ -15,8 +34,12 @@ public class CashRegister {
         //    the price of lunch is added to register
         //    the amount of sold lunch is incremented by one
         //    method returns cashGiven - lunch price 
-        // if not enough money given, all is returned and nothing else happens        
-        return -1;
+        // if not enough money given, all is returned and nothing else happens
+        if (cashGiven >= this.economyPrice) {
+            this.economicalSold++;
+            return payCash(cashGiven, this.economyPrice);
+        }
+        return cashGiven;
     }
 
     public double payGourmet(double cashGiven) {
@@ -26,9 +49,40 @@ public class CashRegister {
         //    the amount of sold lunch is incremented by one
         //    method returns cashGiven - lunch price 
         // if not enough money given, all is returned and nothing else happens
-        return -1;
+        if (cashGiven >= this.gourmetPrice) {
+            this.gourmetSold++;
+            return payCash(cashGiven, this.gourmetPrice);
+        }
+        return cashGiven;
     }
 
+    public boolean payEconomical(LyyraCard card) {
+        // the price of the economical lunch is 2.50 euros
+        // if the balance of the card is at least the price of the lunch:
+        //    the amount of sold lunches is incremented by one
+        //    the method returns true
+        // if not, the method returns false
+        if (card.balance() >= this.economyPrice) {
+            this.economicalSold++;
+            card.pay(this.economyPrice);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean payGourmet(LyyraCard card) {
+        // the price of the gourmet lunch is 4.00 euros
+        // if the balance of the card is at least the price of the lunch:
+        //    the amount of sold lunches is incremented by one
+        //    the method returns true
+        // if not, the method returns false
+        if (card.balance() >= this.gourmetPrice) {
+            this.gourmetSold++;
+            card.pay(gourmetPrice);
+            return true;
+        }
+        return false;
+    }
 
     public String toString() {
         return "money in register " + cashInRegister + " economical lunches sold: " + economicalSold + " gourmet lunches sold: " + gourmetSold;
