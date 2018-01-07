4dd3265083a73efa506a81d61c36a73c4fb58472
Ryan Snead <ryan.w.snead@gmail.com>
2018-01-07 08:57:32 -0500

    Completed exercise 91.

--- 2013-OOProgrammingWithJava-PART1/week5-091.ExtendingMyDate/src/MyDate.java
+++ 2013-OOProgrammingWithJava-PART1/week5-091.ExtendingMyDate/src/MyDate.java
@@ -16,14 +16,17 @@ public class MyDate {
     }
 
     public boolean earlier(MyDate compared) {
+        // first we'll compare years
         if (this.year < compared.year) {
             return true;
         }
 
+        // if the years are the same, we'll compare the months
         if (this.year == compared.year && this.month < compared.month) {
             return true;
         }
 
+        // years and months the same, we'll compare the days
         if (this.year == compared.year && this.month == compared.month
                 && this.day < compared.day) {
             return true;
@@ -32,4 +35,35 @@ public class MyDate {
         return false;
     }
 
+    private int next(int cal, int limit) {
+        if (cal == limit) {
+            return 1;
+        } else {
+            return cal + 1;
+        }
+    }
+
+    public void advance() {
+        this.day = next(this.day, 30);
+        if (this.day == 1) {
+            this.month = next(this.month, 12);
+            if (this.month == 1) {
+                this.year++;
+            }
+        }
+    }
+
+    public void advance(int days) {
+        int day = 0;
+        while (day < days) {
+            this.advance();
+            day++;
+        }
+    }
+
+    public MyDate afterNumberOfDays(int days) {
+        MyDate newMyDate = new MyDate(this.day, this.month, this.year);
+        newMyDate.advance(days);
+        return newMyDate;
+    }
 }
