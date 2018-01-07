6061545e26349e3ab5214596336b00e8bbab9c78
Ryan Snead <ryan.w.snead@gmail.com>
2018-01-07 09:34:17 -0500

    Completed exercise 92.

--- 2013-OOProgrammingWithJava-PART1/week5-092.DifferenceDate/src/MyDate.java
+++ 2013-OOProgrammingWithJava-PART1/week5-092.DifferenceDate/src/MyDate.java
@@ -1,4 +1,6 @@
+
 public class MyDate {
+
     private int day;
     private int month;
     private int year;
@@ -30,4 +32,46 @@ public class MyDate {
         return false;
     }
 
+    private int next(int cal, int limit) {
+        if (cal == limit) {
+            return 1;
+        } else {
+            return cal + 1;
+        }
+    }
+
+    public void advance() {
+        this.day = next(this.day, 30);
+        if (this.day == 1) {
+            this.month = next(this.month, 12);
+            if (this.month == 1) {
+                this.year++;
+            }
+        }
+    }
+
+    public int differenceInYears(MyDate comparedDate) {
+
+        // Line up the two dates in order firstDate and secondDate.
+        MyDate firstDate;
+        MyDate secondDate;
+        if (this.earlier(comparedDate)) {
+            firstDate = new MyDate(this.day, this.month, this.year);
+            secondDate = comparedDate;
+        } else {
+            firstDate = new MyDate(comparedDate.day, comparedDate.month, comparedDate.year);
+            secondDate = this;
+        }
+
+        // Count up the days between comparedDate and this MyDate.
+        int days = 0;
+        while (firstDate.earlier(secondDate)) {
+            firstDate.advance();
+            days++;
+        }
+
+        // Find and return the number of whole years in the counted days.
+        int years = (days / 360);
+        return years;
+    }
 }
