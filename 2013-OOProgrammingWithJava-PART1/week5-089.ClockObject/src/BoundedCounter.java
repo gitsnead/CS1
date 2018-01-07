6772be0f6f2c0820ebabad07463ec17c58746d02
Ryan Snead <ryan.w.snead@gmail.com>
2018-01-07 07:04:49 -0500

    Completed exercise 89.

--- 2013-OOProgrammingWithJava-PART1/week5-089.ClockObject/src/BoundedCounter.java
+++ 2013-OOProgrammingWithJava-PART1/week5-089.ClockObject/src/BoundedCounter.java
@@ -1,4 +1,40 @@
 
 public class BoundedCounter {
     // copy here the class BoundedCounter from last weeks assignment 78 
+
+    private int value;
+    private int upperLimit;
+    private final int lowerLimit = 0;
+
+    public BoundedCounter(int upperLimit) {
+        // write code here
+        this.upperLimit = upperLimit;
+        this.value = 0;
+    }
+
+    public void setValue(int newValue) {
+        if (newValue >= lowerLimit && newValue <= upperLimit) {
+            this.value = newValue;
+        }
+    }
+
+    public int getValue() {
+        // write here code that returns the value
+        return this.value;
+    }
+
+    public void next() {
+        // write code here
+        if (this.value == upperLimit) {
+            this.value = 0;
+        } else {
+            this.value++;
+        }
+    }
+
+    @Override
+    public String toString() {
+        // write code here
+        return String.format("%02d", this.value);
+    }
 }
