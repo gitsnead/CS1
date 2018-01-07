6abc4b4f3f664b72b2df586f8797b5a417ed673c
Ryan Snead <ryan.w.snead@gmail.com>
2018-01-07 06:37:10 -0500

    Completed exercise 88.

--- 2013-OOProgrammingWithJava-PART1/week5-088.Students/src/Main.java
+++ 2013-OOProgrammingWithJava-PART1/week5-088.Students/src/Main.java
@@ -6,5 +6,33 @@ public class Main {
 
     public static void main(String[] args) {
         // write here the main program
+        
+        // Input list of students.
+        Scanner reader = new Scanner(System.in);
+        String studentName;
+        ArrayList<Student> students = new ArrayList<Student>();
+        while (true) {
+            System.out.print("name: ");
+            studentName = reader.nextLine();
+            if (studentName.isEmpty()) {
+                break;
+            }
+            System.out.print("student number: ");
+            students.add(new Student(studentName, reader.nextLine()));
+        }
+        // Print the student list.
+        for(Student student : students) {
+            System.out.println(student.toString());
+        }
+        
+        //String match on student names and print the results.
+        System.out.print("Give search term: ");
+        String searchTerm = reader.nextLine();
+        System.out.println("Result:");
+        for(Student doesStudent : students) {
+            if (doesStudent.getName().contains(searchTerm)) {
+                System.out.println(doesStudent.toString());
+            }
+        }
     }
 }
