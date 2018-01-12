fc69ce533791787ed813c937e82350088c3cd8c2
Ryan Snead <ryan.w.snead@gmail.com>
2018-01-12 10:20:46 -0500

    Completed exercise 98.

--- 2013-OOProgrammingWithJava-PART1/week6-098.CopyAndReverse/src/Main.java
+++ 2013-OOProgrammingWithJava-PART1/week6-098.CopyAndReverse/src/Main.java
@@ -1,9 +1,44 @@
 
+import java.util.Arrays;
 
 public class Main {
 
+    public static int[] copy(int[] array) {
+        int[] arrCopy = new int[array.length];
+        int element;
+        for (int i = 0; i < array.length; i++) {
+            element = array[i];
+            arrCopy[i] = element;
+        }
+        return arrCopy;
+    }
+
+    public static int[] reverseCopy(int[] array) {
+        int[] arrCopy = new int[array.length];
+        int element;
+        for (int i = 0; i < array.length; i++) {
+            element = array[i];
+            arrCopy[array.length -1 - i] = element;
+        }
+        return arrCopy;
+    }
+
     public static void main(String[] args) {
         // write testcode here
+        int[] original = {1, 2, 3, 4};
+        int[] copied = copy(original);
+
+        // change the copied
+        copied[0] = 99;
+
+        // print both
+        System.out.println("original: " + Arrays.toString(original));
+        System.out.println("copied: " + Arrays.toString(copied));
+        int[] reverse = reverseCopy(original);
+
+        // print both
+        System.out.println("original: " + Arrays.toString(original));
+        System.out.println("reversed: " + Arrays.toString(reverse));
     }
-    
+
 }
