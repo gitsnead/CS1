b46bd04e78abb272e9b807c68d748957dc2143cb
Ryan Snead <ryan.w.snead@gmail.com>
2018-01-12 09:02:24 -0500

    Completed exercise 95.

--- 2013-OOProgrammingWithJava-PART1/week6-095.Money/src/Money.java
+++ 2013-OOProgrammingWithJava-PART1/week6-095.Money/src/Money.java
@@ -14,6 +14,35 @@ public class Money {
         this.euros = euros;
         this.cents = cents;
     }
+    
+    public Money plus(Money added) {
+        return new Money(this.euros + added.euros(), this.cents + added.cents());
+    }
+    
+    public Money minus(Money decremented) {
+        int euroAmt = this.euros();
+        int centAmt = this.cents();
+        if (this.cents < decremented.cents) {
+            euroAmt -= 1;
+            centAmt = centAmt + 100 - decremented.cents();
+        } else {
+            centAmt = centAmt - decremented.cents();
+        }
+        if (euroAmt < decremented.euros) {
+            return new Money(0, 0);
+        }
+        return new Money(euroAmt - decremented.euros(), centAmt);
+    }
+    
+    public boolean less(Money compared) {
+        if (this.euros < compared.euros()) {
+            return true;
+        }
+        if (this.euros == compared.euros() && this.cents < compared.cents()) {
+            return true;
+        }
+        return false;
+    }
 
     public int euros() {
         return euros;
